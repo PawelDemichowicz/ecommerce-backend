8de99c66824328ef293cf85b80d8fbdefa9f0679
Pawel9481 <pawel.demichowicz@interia.pl>
2025-03-11 11:13:51 +0100

    Add swagger & upgrade app configs

---
+++ src/main/java/com/ecommerce/configuration/SpringDocConfiguration.java
@@ -0,0 +1,31 @@
+package com.ecommerce.configuration;
+
+import com.ecommerce.EcommerceBackendApplication;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("default")
+                .pathsToMatch("/**")
+                .packagesToScan(EcommerceBackendApplication.class.getPackageName())
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springDocOpenApi() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Ecommerce backend application")
+                        .version("1.0"));
+    }
+}
