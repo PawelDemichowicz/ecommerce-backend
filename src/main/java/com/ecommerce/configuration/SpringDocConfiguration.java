e0aa0f802ba058b117029b1505eee234cccb081b
Pawel9481 <pawel.demichowicz@interia.pl>
2025-03-27 19:41:53 +0100

    Update Swagger configuration for using JWT

--- src/main/java/com/ecommerce/configuration/SpringDocConfiguration.java
+++ src/main/java/com/ecommerce/configuration/SpringDocConfiguration.java
@@ -4,6 +4,8 @@ import com.ecommerce.EcommerceBackendApplication;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,9 +25,16 @@ public class SpringDocConfiguration {
     @Bean
     public OpenAPI springDocOpenApi() {
         return new OpenAPI()
-                .components(new Components())
                 .info(new Info()
                         .title("Ecommerce backend application")
-                        .version("1.0"));
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .name("Bearer Authentication")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
