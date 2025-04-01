8c41c7d08459ad9741d0fffb7c9b73831aaf10ad
Pawel9481 <pawel.demichowicz@interia.pl>
2025-04-01 07:11:01 +0200

    Create validation of ProductDTO

--- src/main/java/com/ecommerce/api/dto/ProductDTO.java
+++ src/main/java/com/ecommerce/api/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.ecommerce.api.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,8 +15,19 @@ import java.math.BigDecimal;
 public class ProductDTO {
 
     Integer id;
+
+    @NotBlank(message = "Product name is required")
+    @Size(min = 3, max = 100, message = "Product name must be between 3 and 100 characters long")
     String name;
+
+    @Size(max = 500, message = "Description cannot exceed 500 characters")
     String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.00", inclusive = false, message = "Invalid price. It must be bigger than 0")
     BigDecimal price;
+
+    @NotNull(message = "Stock quantity is required")
+    @Min(value = 0, message = "Stock quantity cannot be negative")
     Integer stock;
 }
