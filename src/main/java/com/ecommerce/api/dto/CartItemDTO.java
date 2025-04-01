b64bf8fbc0cb4e420ef4cb3592f492c80e26576e
Pawel9481 <pawel.demichowicz@interia.pl>
2025-04-01 07:32:45 +0200

    Create validation of CartItemDTO and security

--- src/main/java/com/ecommerce/api/dto/CartItemDTO.java
+++ src/main/java/com/ecommerce/api/dto/CartItemDTO.java
@@ -1,5 +1,7 @@
 package com.ecommerce.api.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +14,11 @@ import lombok.NoArgsConstructor;
 public class CartItemDTO {
 
     Integer id;
+
+    @NotNull(message = "Quantity is required")
     Integer productId;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 0, message = "Quantity cannot be negative")
     Integer quantity;
 }
