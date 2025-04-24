7d4433b6ab386f1fb6e9ad14cf759f72624f77f3
Pawel9481 <pawel.demichowicz@interia.pl>
2025-04-24 19:06:00 +0200

    Create unit tests for UserService

---
+++ src/test/java/com/ecommerce/business/service/UserServiceTest.java
@@ -0,0 +1,53 @@
+package com.ecommerce.business.service;
+
+import com.ecommerce.business.domain.User;
+import com.ecommerce.business.exception.NotFoundException;
+import com.ecommerce.database.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void shouldReturnUserById() {
+        // given
+        int userId = 1;
+        User user = User.builder().id(userId).build();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // when
+        User result = userService.getUserById(userId);
+
+        // then
+        assertEquals(userId, result.getId());
+    }
+
+    @Test
+    void shouldThrowWhenUserNotFound() {
+        // given
+        int userId = 1;
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // when & then
+        Throwable exception = assertThrows(NotFoundException.class,
+                () -> userService.getUserById(userId));
+        assertEquals("Could not find user by id: [%s]".formatted(userId), exception.getMessage());
+    }
+}
