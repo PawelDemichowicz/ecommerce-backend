798023865510c301cb2458cf9e95b7494c924d45
Pawel9481 <pawel.demichowicz@interia.pl>
2025-04-25 20:55:59 +0200

    Create configuration for integrations DataJpaTest

---
+++ src/test/java/com/ecommerce/integration/repository/configuration/AbstractJpaIT.java
@@ -0,0 +1,14 @@
+package com.ecommerce.integration.repository.configuration;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = DatabaseContainerInitializer.class)
+public abstract class AbstractJpaIT {
+
+}
