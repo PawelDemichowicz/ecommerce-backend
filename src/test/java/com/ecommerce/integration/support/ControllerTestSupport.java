9f756b2635e08886b48a3635a38956af5da51b73
Pawel9481 <pawel.demichowicz@interia.pl>
2025-04-30 12:04:23 +0200

    Add JWT handling in test configuration

--- src/test/java/com/ecommerce/integration/support/ControllerTestSupport.java
+++ src/test/java/com/ecommerce/integration/support/ControllerTestSupport.java
@@ -1,8 +1,13 @@
 package com.ecommerce.integration.support;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.specification.RequestSpecification;
 
 public interface ControllerTestSupport {
 
     ObjectMapper getObjectMapper();
+
+    RequestSpecification getAuthenticatedRequest();
+
+    RequestSpecification getUnauthenticatedRequest();
 }
